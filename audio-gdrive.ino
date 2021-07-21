df45af047c92c63105363488e7368a8beb9e76cb
andrewcgaitskell <andrew@gaitskell.com>
2021-07-21 14:29:12 +0100

    Update audio-gdrive.ino

--- audio-gdrive.ino
+++ audio-gdrive.ino
@@ -20,33 +20,17 @@ WiFiClientSecure  client;
 //const char* password = "PASSWORD";   //your network password
 const char* myDomain = "script.google.com";
 //String myScript = "/macros/s/XXXXXXXXXXXXXXXXXXXXXX/exec";    //Replace with your own url
-String myFilename = "filename=ESP32-CAM.jpg";
-String mimeType = "&mimetype=image/jpeg";
-String myImage = "&data=";
+String myFilename = "filename=ESP32-audio.wav";
+String mimeType = "&mimetype=audio/wav";
+String myAudio = "&data=";
 
 int waitingTime = 30000; //Wait 30 seconds to google response.
 
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
 
 void setup()
 {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   
   Serial.begin(115200);
   delay(10);
@@ -71,65 +55,27 @@ void setup()
   
   Serial.println("");
 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_VGA;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
-  config.jpeg_quality = 10;
-  config.fb_count = 1;
-  
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    delay(1000);
-    ESP.restart();
-  }
 }
 
 boolean enviar = true;
 
 void loop() {
   //if(enviar) {
-    saveCapturedImage();
+    //saveCapturedImage();
     enviar = false;
     delay(60000);
   //}
 }
 
-void saveCapturedImage() {
+void saveCapturedAudio() {
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client;
   
   if (client.connect(myDomain, 443)) {
     Serial.println("Connection successful");
-    
-    camera_fb_t * fb = NULL;
-    fb = esp_camera_fb_get();  
-    if(!fb) {
-      Serial.println("Camera capture failed");
-      delay(1000);
-      ESP.restart();
-      return;
-    }
   
+    // fb is data buffer
+    
     char *input = (char *)fb->buf;
     char output[base64_enc_len(3)];
     String imageFile = "";
@@ -139,9 +85,7 @@ void saveCapturedImage() {
     }
     String Data = myFilename+mimeType+myImage;
     
-    esp_camera_fb_return(fb);
-    
-    Serial.println("Send a captured image to Google Drive.");
+    Serial.println("Send a captured audio to Google Drive.");
     
     client.println("POST " + myScript + " HTTP/1.1");
     client.println("Host: " + String(myDomain));
